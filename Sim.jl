7a2f73a7048086cb96d1f90dbba41f2794907376
Alec Taylor <alectaylor76@gmail.com>
2023-12-22 02:48:33 -0700

    Decided to use an acausal model.
    Have Components for rate, kinetics, and super coiling.

--- Sim.jl
+++ Sim.jl
@@ -2,71 +2,75 @@ using IJulia, ModelingToolkit, DifferentialEquations, Plots, LinearAlgebra
              
 @variables begin
   t
-  repₗ(t)=0         # conc. of reppressor lacI
-  arepₗ(t)=0        # conc. of apolacI
-  indᵢ(t)=11       # conc. of inducer IPTG
-  promₗ(t)=0        # conc. of lac promoter
-  cpromₗ(t)=0       # conc. IPTG bound plac
-  ccₛ(t)=0          # lenggth of the closed mSpinach complex.
-  ecₛ(t)=0          # length of the mSpinach alongation complex.
-  reporterₛ(t)=0    # conc. of the mSplnach reporter
-  rnap(t)=18.931   # conc of RNAP
-  ribo(t) = 0      # conc of ribosomes
-  repₜ(t)=0         # conc of TetR
-  indₐ(t)=11       # conc of aTc
-  arepₜ(t)=0        # conc of apo TetR
-  promₜ(t)=0 
-  cpromₜ(t)=0 
-  ccₘ(t)=0 
-  ecₘ(t)=0 
-  σtₛ(t)=-6 
-  σtₘ(t)=-3 
-  σpₛ(t)=-6 
-  σpₘ(t)=-3
+  repₗ(t)=0 # conc. of reppressor lacI
+  arepₗ(t)=0 # conc. of apolacI
+  indᵢ(t)=11 # conc. of inducer IPTG
+  promₗ(t)=0 # conc. of lac promoter
+  cpromₗ(t)=0 # conc. IPTG bound plac
+  ccₛ(t)=0 # lenggth of the closed mSpinach complex.
+  ecₛ(t)=0 # length of the mSpinach alongation complex.
+  reporterₛ(t)=0 # conc. of the mSplnach reporter
+  rnap(t)=18.931 # conc of RNAP
+  ribo(t) = 0 # conc of ribosomes
+  repₜ(t)=0 # conc of TetR
+  indₐ(t)=11 # conc of aTc
+  arepₜ(t)=0 # conc of apoTetR
+  promₜ(t)=0 # conc ptet
+  cpromₜ(t)=0 # conc bound ptet
+  ccₘ(t)=0 # length of MG closed complex
+  ecₘ(t)=0 # length of MG elongation complex
+  σtₛ(t)=-6 # supercoil state of mSpinach ORF
+  σtₘ(t)=-3 # supercoil state of MG ORF
+  σpₛ(t)=-6 # supercoil state of mSpinach promoter
+  σpₘ(t)=-3 # supercoil state of MG promoter
 end
+
 @parameters begin 
-  h₀=10.5 
-  lₛ=203 
-  lₘ=68 
-  lₛ=150 
-  lₗ=40 
-  lₜ=44 
-  lₚ=2892 
-  kσₘₘ=50 
-  gyr₀=12 
-  topo₀=2 
-  kₗ₊=7e-2 
-  kᵣ=550 
-  kₜₓₚ=85 
-  kₜₓ=kₜₓₚ/105.5 
-  kₒₚₑₙ=0.04 
-  kₗₑₐₖ=0.02 
-  ρₗ=0 
-  ρₜ=0 
-  kₜₗ=21/(714+675) 
-  kᵪ=1/90 
-  kᵣ=1/170 
-  k₁₆=0.75 
-  kbgl=1.25 
-  δₛ=log(2)/(30*60) 
-  δₘ=log(2)/(60*60)
-  τ=0.5 
-  γ=0.5 
-  σ₀=-0.065 
-  kgyrₘₘ=200 
-  kaₗ=6e3 kuaₗ=1 
-  kbindₗ=10 
-  kuₗ=0.022 
-  kaₜ=6e3 
-  kuaₜ=1 
-  kbindₜ=10 
-  kuₜ=0.022 
-  δₚ=0
+  h₀=10.5 # BP per right hand turn of B-DNA
+  lₛ=203 # Length of intergenic region
+  lₘ=68 # Length MS
+  lₛ=150 # Length of MS
+  
+  lₜ=44 # Length of ptet
+  lₚ=2892 # Plasmid Length
+  kσₘₘ=50 # Michaelis-Menten constant for supercoiling hillfunctions
+  gyr₀=12 # Concentration Gyrase
+  topo₀=2 # Conc. Topoisomerase
+  kₗ₊=7e-2 # Rate forward transcription
+  kᵣ=550 # Reverse transcription rate
+  kₜₓₚ=85 # Perbase transcription rate
+  kₜₓ=kₜₓₚ/105.5 # Average transcription rate
+  kₒₚₑₙ=0.04 # Rate of open complex formation
+  kₗₑₐₖ=0.02 # Rate of terminator escaping transcriptio
+  ρₗ=0 # Rate of lacI production
+  ρₜ=0 # Rate of tetR production
+  kₜₗ=21/(714+675) # RFP/CFP average translation rate
+  kᵪ=1/90 # CFP maturation rate
+  kᵣ=1/170 # RFP maturation rate
+  k₁₆=0.75 # BCD16 binding rate (relative)
+  kbgl=1.25 # BCDbgl binding rate (relative)
+  δₛ=log(2)/(30*60) # mSpinach degredation rate
+  δₘ=log(2)/(60*60) # MG degredation rate
+  τ=0.5 # Rate of topoisomerase activity
+  γ=0.5 # Rate of Gyrase activity
+  σ₀=-0.065 # standard supercoil state
+  kgyrₘₘ=200 # Michaelis-Menten constant for gyrase
+  kaₗ=6e3 # Rate of DNA-free apolacI IPTG binding
+  kuaₗ=1 # Rate of apolacI IPTG disassociation
+  kbindₗ=10 # lacI-promoter asossiation rate
+  kuₗ=0.022 # lacI-promoter disassociation rate
+  kaₜ=6e3 # aTc-TetR association rate
+  kuaₜ=1 # aTc-TetR disassociation rate
+  kbindₜ=10 # tetR-DNA association rate
+  kuₜ=0.022 # tetR-DNA disassociation rate
+  δₚ=0 # Average protein degredation rate
 end
 
-xdot=Differential(t)
+D=Differential(t)
 
-
-
-
-@begin
+@mtkmodel rates begin
+    @parameters begin
+        lₗ = 40, [description="Length of plac [bp]"]
+          
+    end
+end
