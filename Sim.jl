a1a01aa3d54fd37c9342294b8c70683178e38a1f
Alec Taylor <alectaylor76@gmail.com>
2023-12-27 16:15:41 -0700

    Added Reporter Dynamical System.

--- Sim.jl
+++ Sim.jl
@@ -1,46 +1,31 @@
 using IJulia, ModelingToolkit, DifferentialEquations, Plots, Unitful
 
 @variables begin
-  t
   ribo(t) = 0 # conc of ribosomes
 end
 
 @parameters begin 
-  h₀=10.5 # BP per right hand turn of B-DNA
-  lₛ=203 # Length of intergenic region
-  lₘ=68 # Length MS
-  lₛ=150 # Length of MS
+  t
+  lᵢ=203 # Length of intergenic region
+  lₘ=68 # Length MG
+  lₛ=150 # Length of mSpinach
   
   lₜ=44 # Length of ptet
 
-  
-  
   kₗ₊=7e-2 # Rate forward transcription
   kᵣ=550 # Reverse transcription rate
   kₜₓₚ=85 # Perbase transcription rate
   kₜₓ=kₜₓₚ/105.5 # Average transcription rate
   
   kₗₑₐₖ=0.02 # Rate of terminator escaping transcriptio
-  ρₗ=0 # Rate of lacI production
-  ρₜ=0 # Rate of tetR production
+
   kₜₗ=21/(714+675) # RFP/CFP average translation rate
   kᵪ=1/90 # CFP maturation rate
-  kᵣ=1/170 # RFP maturation rate
+
   k₁₆=0.75 # BCD16 binding rate (relative)
   kbgl=1.25 # BCDbgl binding rate (relative)
 
-  
   σ₀=-0.065 # standard supercoil state
-  
-  kaₗ=6e3 # Rate of DNA-free apolacI IPTG binding
-  kuaₗ=1 # Rate of apolacI IPTG disassociation
-  kbindₗ=10 # lacI-promoter asossiation rate
-  kuₗ=0.022 # lacI-promoter disassociation rate
-  kaₜ=6e3 # aTc-TetR association rate
-  kuaₜ=1 # aTc-TetR disassociation rate
-  kbindₜ=10 # tetR-DNA association rate
-  kuₜ=0.022 # tetR-DNA disassociation rate
-  δₚ=0 # Average protein degredation rate
 end
 
 @variables t
@@ -62,7 +47,7 @@ D=Differential(t)
       ecₘ(t)=0, [description="Number of MG closed dna comlexes"]
       rnap(t)=0, [description="conc RNAP", unit=u"nM"]
       promₗ(t)=0, [description="conc plac", unit=u"nM"]
-      cprom(t)=0, [description="conc plac-lacI complex" ,unit=u"nM"]
+      cpromₗ(t)=0, [description="conc plac-lacI complex" ,unit=u"nM"]
       promₜ(t)=0, [description="conc pTet", unit=u"nM"]
       cpromₜ(t)=0, [description="conc pTet-TetR complex", unit=u"nM"]
       repₗ(t)=0, [description="conc lacI Repressor", unit=u"nM"] 
@@ -82,24 +67,24 @@ D=Differential(t)
 end
 
 @mtkmodel rates begin
+  @variables begin
+    σpₗ(t)
+    σtₗ(t)
+    σpₘ(t)
+    σtₘ(t)
+  end
+
   @parameters begin
       lₗ = 40, [description="Length of plac"] #, unit=ub"bp"]
       lₚ=2892, [description="Length of plasmid"]#, unit=ub"bp"]
       σ₀= -0.065, [description="Length of plac"]#, unit=ub"bp"]
       kinitₘ = 7e-2, [description="Max initiation rate"]#, unit=u"nt/s"]
       kelongₘ = 7e-2, [description="Max elomgation rate"]#, unit=u"nt/s"]
-      kσₘₘ= 50, [description="Michaelis-Menten constant for supercoiling hillfunctions", unit=u"μM"]
+      kσₘₘ= 50, [description="MM constant for supercoiling hillfunctions",unit=u"μM"]
       σspₗ = σ₀*lₚ/lₗ
       σstₗ = σ₀*lₚ/lₛ
       σspₘ = σ₀*lₚ/lₜ
       σstₘ = σ₀*lₚ/lₘ
-  end
-
-  @variables begin
-      σpₗ(t)
-      σtₗ(t)
-      σpₘ(t)
-      σtₘ(t)
   end
 
   @equations begin
@@ -116,23 +101,57 @@ end
       cLaws = conservationLaws(t)
   end
   @variables begin
-      reporterₛ(t)=0
-      reporterₘ(t)=0
+      reporterₛ(t)=0, [description="mSpinach Transcript"]
+      reporterₘ(t)=0, [description="MG Transcript"]
   end
   @parameters begin
-      δₛ=log(2)/(30*60) # mSpinach degredation rate
-      δₘ=log(2)/(60*60) # MG degredation rate
-      kₒₚₑₙ=0.04 # Rate of open complex formation
-      
+    δₛ=log(2)/(30*60), [description = "mSpinach degredation rate"]
+    δₘ=log(2)/(60*60), [description = "MG degredation rate"]
+    δₚ=0,[description="Average protein degredation rate"]
+    kₒₚₑₙ=0.04, [description = "Rate of open complex formation"]
+    kᵣ=1/170, [description = "RFP maturation rate"]
+    kaₗ=6e3, [description="Rate of DNA-free apolacI IPTG binding"]
+    kuaₗ=1,[description="Rate of apolacI IPTG disassociation"]
+    kbindₗ=10, [description="lacI-promoter asossiation rate"]
+    kuₗ=0.022, [description ="lacI-promoter disassociation rate"]
+    kaₜ=6e3, [description = "aTc-TetR association rate"]
+    kuaₜ=1, [description="aTc-TetR disassociation rate"]
+    kbindₜ=10, [description="tetR-DNA association rate"]
+    kuₜ=0.022, [description = "tetR-DNA disassociation rate"]
+    ρₗ=0, [description="Rate of lacI production"]
+    ρₜ=0, [description="Rate of tetR production"]    
   end
+
+  @equations begin
+    D(reporterₛ) ~ r.kinitₗ*cLaws.ecₛ - δₛ*reporterₛ
+    D(reporterₘ) ~ r.kinitₘ*cLaws.ecₘ - δₘ*reporterₘ
+    D(cLaws.ecₛ) ~ kₒₚₑₙ*cLaws.ccₛ - r.kinitₗ*cLaws.ecₛ
+    D(cLaws.ecₘ) ~ kₒₚₑₙ*cLaws.ccₘ - r.kinitₘ*cLaws.ecₘ
+    D(cLaws.ccₛ) ~ r.kelongₗ*(rnapᵗ-cLaws.ecₛ-cLaws.ecₘ-cLaws.ccₛ-cLaws.ccₘ)...
+                  *(pₗᵗ-cLaws.ccₛ-cLaws.ecₛ-cpromₗ)-(r.kelongₗ-kₒₚₑₙ)*cLaws.ccₛ
+    D(cLaws.ccₘ) ~ r.kelongₘ*(rnapᵗ-cLaws.ecₘ-cLaws.ecₛ-cLaws.ccₛ-cLaws.ccₘ)...
+                  *(pₘᵗ-cLaws.ccₘ-cLaws.ecₘ-cpromₜ)-(r.kelongₘ-kₒₚₑₙ)*cLaws.ccₘ
+    D(repₗ) ~ ρₗ+kuaₗ*(indᵢᵗ-indᵢ)+kuₗ(repₗᵗ-repₗ-indᵢᵗ-indᵢ)-kaₗ*repₗ*indᵢ-kbindₗ*repₗ-σₚ*repₗ
+    D(repₜ) ~ ρₜ+kuaₜ*(indₐᵗ-indₐ)*kuₜ(repₜᵗ-repₜ-indₐᵗ-indₐ)-kaₜ*repₜ*indₐ-kbindₜ*repₜ-σₚ*repₜ
+    D(indᵢ) ~ kaₗ*(repₗ+cpromₗ)*indᵢ+kuaₗ(repₗᵗ-repₗ-cpromₗ)
+    D(indₐ) ~ kaₜ*(repₜ+cpromₜ)*indₐ+kuaₜ(repₜᵗ-repₜ-cpromₜ)
 end
 
 @mtkmodel σDynamics begin
+  @components begin
+    exdy = reporterDynamics
+  end
   @variables begin
-  σtₛ(t)=-6 # supercoil state of mSpinach ORF
-  σtₘ(t)=-3 # supercoil state of MG ORF
-  σpₛ(t)=-6 # supercoil state of mSpinach promoter
-  σpₘ(t)=-3 # supercoil state of MG promoter   
+    σtₛ(t)=-6 # supercoil state of mSpinach ORF
+    σtₘ(t)=-3 # supercoil state of MG ORF
+    σpₛ(t)=-6 # supercoil state of mSpinach promoter
+    σpₘ(t)=-3 # supercoil state of MG promoter   
+  end
+
+  @parameters begin
+    h₀=10.5, [description="BP per right hand turn of B-DNA"]
+    nfₛ = l
+
   end
 end
 
