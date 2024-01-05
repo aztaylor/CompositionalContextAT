24531ceb2ff4e06bfb951f8c87cc0bd26d5e4f3c
Alec Taylor <alectaylor76@gmail.com>
2024-01-04 22:12:56 -0800

    Added notes and almost fixed units.
    It looks like the conditionals are causing issues in the maintanance function.

--- Sim.jl
+++ Sim.jl
@@ -1,6 +1,6 @@
 using IJulia, ModelingToolkit, DifferentialEquations, Plots, Unitful, UnitfulAngles
 
-@parameters t
+@parameters t, [unit=u"s"]
 D = Differential(t)
 
 @mtkmodel rates begin
@@ -10,10 +10,10 @@ D = Differential(t)
     lₛ=240, [description="Length of mSpinach and T500 terminator", unit=u"bp"]
     lₘ=63,  [description="Length of MG and T500 terminator", unit=u"bp"]
     lₚ=2892, [description="Length of plasmid", unit=u"bp"]
-    kinitₘₐₓ = 7e-2, [description="Max initiation rate", unit=u"s^-1"]
+    kinitₘₐₓ = 7e-2, [description="Max initiation rate", unit=u"nM*s^-1"]
     kelongₘₐₓ = 7e-2, [description="Max elongation rate", unit=u"s^-1"]
     kσₘₘ= 50, [description="MM constant for supercoiling hillfunctions",unit=u"μM"]
-    kₒₚₑₙ=0.04, [description = "Rate of open complex formation", unit= u"turn*s^-1"]
+    kₒₚₑₙ=0.04, [description = "Rate of open complex formation", unit= u"s^-1"]
     kᵣ=1/170, [description = "RFP maturation rate", unit= u"s^-1"]
     kaₗ=6e3, [description="Rate of DNA-free apolacI IPTG binding", unit= u"s^-1"]
     kuaₗ=1,[description="Rate of apolacI IPTG disassociation", unit = u"s^-1"]
@@ -29,16 +29,16 @@ D = Differential(t)
     δₘ=log(2)/(60*60), [description = "MG degredation rate", unit= u"s^-1"]
     δₚ=0,[description="Average protein degredation rate", unit= u"s^-1"]
     σ₀= -0.065, [description="Natural B-form DNA supercoil state", unit=u"turn*bp^-1"] 
-    σspₗ = σ₀*lₚ/lₗ, [description="Approximate Optimal supercoiling density, plac"]
-    σstₛ = σ₀*lₚ/lₛ, [description="Approximate Optimal supercoiling density, plac"]
-    σspₜ = σ₀*lₚ/lₜ, [description="Approximate Optimal supercoiling density, pTet"]
-    σstₘ = σ₀*lₚ/lₘ, [description="Approximate Optimal supercoiling density, pTet"]
+    σspₗ = σ₀*lₚ/lₗ, [description="Approximate Optimal supercoiling density, plac", unit=u"bp*turn^-1"]
+    σstₛ = σ₀*lₚ/lₛ, [description="Approximate Optimal supercoiling density, plac", unit=u"bp*turn^-1"]
+    σspₜ = σ₀*lₚ/lₜ, [description="Approximate Optimal supercoiling density, pTet", unit=u"bp*turn^-1"]
+    σstₘ = σ₀*lₚ/lₘ, [description="Approximate Optimal supercoiling density, pTet", unit=u"bp*turn^-1"]
 
   end
   @variables begin
-    σtₛ(t)=-6, [description="supercoil state of mSpinach ORF"]
+    σtₛ(t)=-6, [description="supercoil state of mSpinach ORF", unit="bp*turn^-1"]
     σtₘ(t)=-3, [description="supercoil state of MG ORF"]
-    σpₗ(t)=-6, [description="supercoil state of mSpinach promoter"]
+    σpₗ(t)=-6, [description="supercoil state of mSpinach promoter", unit="bp*turn^-1"]
     σpₜ(t)=-3, [description="supercoil state of MG promoter"]
     kinitₗ(t) = 0.5, [description="transcription initiation rate plac", unit=u"s^-1"]
     kinitₜ(t) = 0.5, [description="transcription initiation rate pTet", unit=u"s^-1"]
@@ -56,10 +56,10 @@ end
 @mtkmodel dnaComplexDynamics begin
   @extend rates()
   @parameters begin
-    rnapᵗᵒᵗ=0.0, [description="Total RNAP, in myTXTL no degredataion", unit=u"nM", connect = Flow]
-    promₗᵗᵒᵗ=0.0, [description="Total lac promoter", connect = Flow, unit=u"nM"]
-    promₜᵗᵒᵗ=0.0, [description="Total Tet Promoter", connect = Flow, unit=u"nM"]
-    reprₗᵗᵒᵗ=0.0, [description="Total Lac Repressor, in myTXTL no degredataion", connect = Flow, unit=u"nM"]
+    rnapᵗᵒᵗ=18.931e3, [description="Total RNAP, in myTXTL no degredataion", unit=u"nM", connect = Flow]
+    promₗᵗᵒᵗ=11.0, [description="Total lac promoter", connect = Flow, unit=u"nM"]
+    promₜᵗᵒᵗ=11.0, [description="Total Tet Promoter", connect = Flow, unit=u"nM"]
+    reprₗᵗᵒᵗ=10, [description="Total Lac Repressor, in myTXTL no degredataion", connect = Flow, unit=u"nM"]
     reprₜᵗᵒᵗ=0.0, [description="Total Tet Repressor, in myTXTL no degredataion", connect = Flow, unit=u"nM"]
     indᵢᵗᵒᵗ=0.0, [description="Total IPTG, not metabolized by the reaction volume", connect = Flow, unit=u"nM"]
     indₐᵗᵒᵗ=0.0, [description="Total aTc, not metabolized by the reaction volume", connect = Flow, unit=u"nM"]
@@ -71,8 +71,8 @@ end
     ccₘ(t)=0, [description="Number of MG closed dna comlexes", unit=u"nM"]
     promₗ(t)=promₗᵗᵒᵗ, [description="conc plac" ,unit=u"nM"]
     promₜ(t)=promₜᵗᵒᵗ,[description="conc pTet" ,unit=u"nM"]
-    cpromₗ(t)=0, [description="conc plac-lacI complex" ,unit=u"nM"]
-    cpromₜ(t)=0, [description="conc pTet-TetR complex", unit=u"nM"]
+    promₗc(t)=0, [description="conc plac-lacI complex" ,unit=u"nM"]
+    promₜc(t)=0, [description="conc pTet-TetR complex", unit=u"nM"]
     reprₗ(t)=0, [description="conc LacI Repressor", unit=u"nM"]
     reprₜ(t)=0, [description="conc TetR Repressor", unit=u"nM"]   
     areprₗ(t)=0, [description="conc apo LacI", unit=u"nM"]
@@ -83,10 +83,10 @@ end
   end
   @equations begin
     rnapᵗᵒᵗ ~ rnap+ecₛ+ecₘ+ccₛ+ccₘ
-    promₗᵗᵒᵗ ~ promₗ+ccₛ+ecₛ+cpromₗ
-    promₜᵗᵒᵗ ~ promₜ+ccₘ+ecₘ+cpromₜ
-    indᵢᵗᵒᵗ ~ indᵢ+areprₗ+cpromₗ
-    indₐᵗᵒᵗ ~ indₐ+areprₜ+cpromₜ
+    promₗᵗᵒᵗ ~ promₗ+ccₛ+ecₛ+promₗc
+    promₜᵗᵒᵗ ~ promₜ+ccₘ+ecₘ+promₜc
+    indᵢᵗᵒᵗ ~ indᵢ+areprₗ+promₗc
+    indₐᵗᵒᵗ ~ indₐ+areprₜ+promₜc
   end
 end 
 
@@ -102,12 +102,12 @@ end
     D(reporterₘ)~kinitₜ*ecₘ-δₘ*reporterₘ
     D(ecₛ)~kₒₚₑₙ*ccₛ-kinitₗ*ecₛ
     D(ecₘ)~kₒₚₑₙ*ccₘ-kinitₜ*ecₘ
-    D(ccₛ)~kelongₛ*(rnapᵗᵒᵗ-ecₛ-ecₘ-ccₛ-ccₘ)*(promₗᵗᵒᵗ-ccₛ-ecₛ-cpromₗ)-(kelongₛ-kₒₚₑₙ)*ccₛ
-    D(ccₘ)~kelongₘ*(rnapᵗᵒᵗ-ecₘ-ecₛ-ccₛ-ccₘ)*(promₜᵗᵒᵗ-ccₘ-ecₘ-cpromₜ)-(kelongₘ-kₒₚₑₙ)*ccₘ
+    D(ccₛ)~kelongₛ*(rnapᵗᵒᵗ-ecₛ-ecₘ-ccₛ-ccₘ)*(promₗᵗᵒᵗ-ccₛ-ecₛ-promₗc)-(kelongₛ-kₒₚₑₙ)*ccₛ
+    D(ccₘ)~kelongₘ*(rnapᵗᵒᵗ-ecₘ-ecₛ-ccₛ-ccₘ)*(promₜᵗᵒᵗ-ccₘ-ecₘ-promₜc)-(kelongₘ-kₒₚₑₙ)*ccₘ
     D(reprₗ)~ρₗ+kuaₗ*(indᵢᵗᵒᵗ-indᵢ)+kuₗ*(reprₗᵗᵒᵗ-reprₗ-indᵢᵗᵒᵗ-indᵢ)-kaₗ*reprₗ*indᵢ-kbindₗ*reprₗ-δₚ*reprₗ
     D(reprₜ)~ρₜ+kuaₜ*(indₐᵗᵒᵗ-indₐ)*kuₜ*(reprₜᵗᵒᵗ-reprₜ-indₐᵗᵒᵗ-indₐ)-kaₜ*reprₜ*indₐ-kbindₜ*reprₜ-δₚ*reprₜ
-    D(indᵢ)~kaₗ*(reprₗ+cpromₗ)*indᵢ+kuaₗ*(reprₗᵗᵒᵗ-reprₗ-cpromₗ)
-    D(indₐ)~kaₜ*(reprₜ+cpromₜ)*indₐ+kuaₜ*(reprₜᵗᵒᵗ-reprₜ-cpromₜ)
+    D(indᵢ)~kaₗ*(reprₗ+promₗc)*indᵢ+kuaₗ*(reprₗᵗᵒᵗ-reprₗ-promₗc)
+    D(indₐ)~kaₜ*(reprₜ+promₜc)*indₐ+kuaₜ*(reprₜᵗᵒᵗ-reprₜ-promₜc)
   end
 end
 
@@ -115,43 +115,44 @@ end
   @extend reporterDynamics()
   @parameters begin
     h₀= 10.5, [description="basepairs per right-hand turn", unit=u"bp*turn"]
+    σ₀=-0.065, [description="standard supercoil state", unit = u"bp*turn^-1"]
   end
   @variables begin  
-    σtₛ(t) = σ₀
-    σtₘ(t) = σ₀
+    σtₛ(t) = σ₀, [description="supercoiling density of mSpinach ORF+Term", unit=u"bp*turn^-1"]
+    σtₘ(t) = σ₀,[description="supercoiling density of MG ORF+Term", unit=u"bp*turn^-1"]
     nfₛ(t)=100, [description="length of promoter, mSpinach ORF, terminator and available intergenic space", unit=u"bp"]
     nfₘ(t)=100, [description="length of promoter, MG ORF, terminator and available intergenic space", unit=u"bp"]
   end
   @equations begin
     Δₖᵢₙₖ = (σtₛ+σtₘ)*h₀
-    nfₛ = max((lₗ+lₛ+lᵢ/2(promₜ/(promₜ+cpromₜ))+(lᵢ/2+lₘ)*cpromₜ/(promₜ+cpromₜ)-Δₖᵢₙₖ),0) 
-    nfₘ = max((lₜ+lₘ+lᵢ/2(promₗ/(promₗ-cpromₗ))+(lᵢ/2+lₛ)*cpromₗ/(promₗ+cpromₗ)-Δₖᵢₙₖ),0)
+    nfₛ = max((lₗ+lₛ+lᵢ/2(promₜ/(promₜ+promₜc))+(lᵢ/2+lₘ)*promₜc/(promₜ+promₜc)-Δₖᵢₙₖ),0) 
+    nfₘ = max((lₜ+lₘ+lᵢ/2(promₗ/(promₗ-promₗc))+(lᵢ/2+lₛ)*promₗc/(promₗ+promₗc)-Δₖᵢₙₖ),0)
   end
 end
 
 @mtkmodel σMaintenanceDynamics begin
   @extend reporterDynamics()
+  @extend nᵢ()
   @parameters begin
     gyr₀=18.93, [description="Concentration Gyrase", unit=u"μM"]
     topo₀=2, [description="Conc Topoisomerase", unit=u"μM"]
     τ=0.5, [description="Rate of topoisomerase activity", unit=u"turn*s^-1"]
     γ=0.5, [description="Rate of Gyrase activit", unit=u"turn*s^-1"]
     kgyrₘₘ=200, [description="Michaelis-Menten constant for gyrase", unit=u"μM"]
-    σ₀=-0.065, [description="standard supercoil state"]
   end 
   @variables begin
-    σpₗ₊(t) = 0, [description="Decomposition of the plac supercoiling state into strictly positive parts"] 
-    σpₗ₋(t) = 0, [description="Decomposition of the plac supercoiling state into strictly negative parts"]
-    σtₛ₊(t) = 0, [description="Decomposition of the mSpinach supercoiling state into strictly positive parts"]
-    σtₛ₋(t) = 0, [description="Decomposition of the mSpinach supercoiling state into strictly negative parts"]
-    σpₜ₊(t) = 0, [description="Decomposition of the pTet supercoiling state into strictly positive parts"]
-    σpₜ₋(t) = 0, [description="Decomposition of the pTet supercoiling state into strictly negative parts"]
-    σtₘ₊(t) = 0, [description="Decomposition of the MG supercoiling state into strictly positive parts"]
-    σtₘ₋(t) = 0, [description="Decomposition of the MG supercoiling state into strictly negative parts"]
-    mpₗ(t) = 0, [description="Maintenance Dynamics from topoisomerase and Gyrase"]
-    mtₛ(t) = 0, [description="Maintenance Dynamics from topoisomerase and Gyrase"]
-    mpₜ(t) = 0, [description="Maintenance Dynamics from topoisomerase and Gyrase"]
-    mtₘ(t) = 0, [description="Maintenance Dynamics from topoisomerase and Gyrase"]
+    σpₗ₊(t) = 0, [description="Decomposition of the plac supercoiling state into strictly positive parts", unit = u"bp*turn^-1"] 
+    σpₗ₋(t) = 0, [description="Decomposition of the plac supercoiling state into strictly negative parts", unit = u"bp*turn^-1"]
+    σtₛ₊(t) = 0, [description="Decomposition of the mSpinach supercoiling state into strictly positive parts", unit = u"bp*turn^-1"]
+    σtₛ₋(t) = 0, [description="Decomposition of the mSpinach supercoiling state into strictly negative parts", unit = u"bp*turn^-1"]
+    σpₜ₊(t) = 0, [description="Decomposition of the pTet supercoiling state into strictly positive parts", unit = u"bp*turn^-1"]
+    σpₜ₋(t) = 0, [description="Decomposition of the pTet supercoiling state into strictly negative parts", unit = u"bp*turn^-1"]
+    σtₘ₊(t) = 0, [description="Decomposition of the MG supercoiling state into strictly positive parts", unit = u"bp*turn^-1"]
+    σtₘ₋(t) = 0, [description="Decomposition of the MG supercoiling state into strictly negative parts", unit = u"bp*turn^-1"]
+    mpₗ(t) = 0, [description="Maintenance Dynamics from topoisomerase and Gyrase", unit = u"s^-1"]
+    mtₛ(t) = 0, [description="Maintenance Dynamics from topoisomerase and Gyrase", unit = u"s^-1"]
+    mpₜ(t) = 0, [description="Maintenance Dynamics from topoisomerase and Gyrase", unit = u"s^-1"]
+    mtₘ(t) = 0, [description="Maintenance Dynamics from topoisomerase and Gyrase", unit = u"s^-1"]
   end  
   @equations begin
     if σpₗ > 0
@@ -174,10 +175,10 @@ end
        σtₘ₋ = σtₘ₋+σtₘ
     end
       
-    mpₗ~topo₀*τ*(σpₗ₋)/kσₘₘ/(σ₀+(σpₗ-σ₀)^2)\kσₘₘ*gyr₀*γ*(σpₗ₊)/kσₘₘ/(σ₀+(σpₗ-σ₀)^2)/kσₘₘ
-    mtₛ~topo₀*τ*(σtₛ₋)/kσₘₘ/(σ₀+(σtₛ-σ₀)^2)/kσₘₘ*gyr₀*γ*(σtₛ₊)/kσₘₘ/(σ₀+(σtₛ-σ₀)^2)/kσₘₘ
-    mpₜ~topo₀*τ*(σpₜ₋)/kσₘₘ/(σ₀+(σpₜ-σ₀)^2)*gyr₀*γ*(σpₜ₊)/kσₘₘ/(σ₀+(σpₜ-σ₀)^2)/kσₘₘ
-    mtₘ~topo₀*τ*(σtₘ₋)/kσₘₘ/(σ₀+(σtₘ-σ₀)^2)*gyr₀*γ*(σtₘ₊)/kσₘₘ/(σ₀+(σtₘ-σ₀)^2)/kσₘₘ
+    mpₗ~topo₀*τ*(σpₗ₋)/kσₘₘ/(σ₀+(σpₗ-σ₀)^2)/kσₘₘ+gyr₀*γ*(σpₗ₊)/kσₘₘ/(σ₀+(σpₗ-σ₀)^2)/kσₘₘ
+    mtₛ~topo₀*τ*(σtₛ₋)/kσₘₘ/(σ₀+(σtₛ-σ₀)^2)/kσₘₘ+gyr₀*γ*(σtₛ₊)/kσₘₘ/(σ₀+(σtₛ-σ₀)^2)/kσₘₘ
+    mpₜ~topo₀*τ*(σpₜ₋)/kσₘₘ/(σ₀+(σpₜ-σ₀)^2)/kσₘₘ+gyr₀*γ*(σpₜ₊)/kσₘₘ/(σ₀+(σpₜ-σ₀)^2)/kσₘₘ
+    mtₘ~topo₀*τ*(σtₘ₋)/kσₘₘ/(σ₀+(σtₘ-σ₀)^2)\kσₘₘ+gyr₀*γ*(σtₘ₊)/kσₘₘ/(σ₀+(σtₘ-σ₀)^2)/kσₘₘ
   end
 end
 
