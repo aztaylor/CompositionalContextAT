4b8b827bc4acd1d3eb7e4edd4455355c5314a931
Alec Taylor <alectaylor76@gmail.com>
2024-01-10 18:18:50 -0800

    Finished Consrtrcuting The system, I hope.

--- CatalystModel/model.jl
+++ CatalystModel/model.jl
@@ -1,70 +1,73 @@
-using Catalyst, ModelingToolkit, Plots, Latexify, Main.UnitfulBio, Unitful, IfElse, UnitfulAngles
+using Catalyst, ModelingToolkit, Plots, Latexify# , Main.UnitfulBio], # ful], # fulAngles
 # Have decided that catalyst seems like the best approach over raw
 # dogging some Modeling Tool Kit. Saves some lines for sure.
 # I have no idea if this is right but I believe so.
-@variables t [unit = u"s"]
-D = Differential(t)
 @parameters begin
-
-  pₗᵗᵒᵗ=11.0,          [description="Total lac promoter", connect=Flow, unit=u"nM"]
-  pₜᵗᵒᵗ=11.0,         [description="Total Tet Promoter", connect=Flow, unit=u"nM"]
-  h₀ = 10.4,         [description="basepair per right hand turn",  unit=u"bp*turn^-1"]
-  lₗ=40,              [description="Length of plac", unit=u"bp"]
-  lₜ=44,              [description="Length of ptet", unit=u"bp"]
-  lₛ=240,             [description="Length of mSpinach and T500 terminator", unit=u"bp"]
-  lₘ=63,              [description="Length of MG and T500 terminator", unit=u"bp"]
-  lₚ=2892,            [description="Length of plasmid", unit=u"bp"]
-  lᵢ = 150,           [description="Length of intergenic region", unit=u"bp"]
-  kσₘₘ = 50e-3,       [description="MM constant for supercoiling hillfunctions",unit=u"nM"]
-  kₒₚₑₙ=0.04,          [description="Rate of open complex formation", unit=u"s^-1"]
-  kᵣ=1/170,           [description="RFP maturation rate", unit=u"s^-1"]
-  kaₗ=6e3,             [description="Rate of DNA-free apolacI IPTG binding", unit=u"nM^-1*s^-1"]
-  kuaₗ=1,              [description="Rate of apolacI IPTG disassociation", unit =u"s^-1"]
-  kbindₗ=10,           [description="lacI-promoter asossiation rate", unit=u"s^-1"]
-  kuₗ=0.022,           [description="lacI-promoter disassociation rate", unit=u"s^-1"]
-  kaₜ=6e3,             [description="aTc-TetR association rate", unit=u"nM^-1*s^-1"]
-  kuaₜ=1,              [description="aTc-TetR disassociation rate", unit=u"s^-1"]
-  kbindₜ=10,           [description="tetR-DNA association rate", unit=u"s^-1"]
-  kuₜ=0.022,           [description="tetR-DNA disassociation rate", unit=u"s^-1"]
-  ρₗ=0,                [description="Rate of lacI production", unit=u"nM*s^-1"]
-  ρₜ=0,                [description="Rate of tetR production", unit=u"nM*s^-1"]
-  δₛ=log(2)/(30*60),   [description="mSpinach degredation rate", unit=u"s^-1"]
-  δₘ=log(2)/(60*60),   [description="MG degredation rate", unit=u"s^-1"]
-  δₚ=0,                [description="Average protein degredation rate", unit=u"s^-1"]
-  σ₀=-0.065,           [description="Natural B-form DNA supercoil state", unit=u"turn*bp^-1"] 
-  σspₗ = σ₀*lₚ/lₗ,       [description="Approximate Optimal supercoiling density, plac"]
-  σstₛ = σ₀*lₚ/lₛ,      [description="Approximate Optimal supercoiling density, plac"]
-  σspₜ = σ₀*lₚ/lₜ,      [description="Approximate Optimal supercoiling density, pTet"]
-  σstₘ = σ₀*lₚ/lₘ,     [description="Approximate Optimal supercoiling density, pTet"]
-  fudge = 1,           [description="Fudge Factor", unit=u"nM"]
-  gyr₀=18.93, [description="Concentration Gyrase", unit=u"μM"]
-  topo₀=2, [description="Conc Topoisomerase", unit=u"μM"]
-  τ=0.5, [description="Rate of topoisomerase activity", unit=u"turn*s^-1"]
-  γ=0.5, [description="Rate of Gyrase activit", unit=u"turn*s^-1"]
-  kσₘₘ=200, [description="Michaelis-Menten constant for gyrase", unit=u"μM"]
+  pₗᵗᵒᵗ=11.0 #,         [description="Total lac promoter", connect=Flow], # =u"nM"]
+  pₜᵗᵒᵗ=11.0 #,         [description="Total Tet Promoter", connect=Flow], # =u"nM"]
+  h₀ = 10.4 #,          [description="basepair per right hand turn"], #,  unit=u"bp*turn^-1"]
+  lₗ=40 #,              [description="Length of plac"], # =u"bp"]
+  lₜ=44 #,              [description="Length of ptet"], # =u"bp"]
+  lₛ=240 #,             [description="Length of mSpinach and T500 terminator"], # =u"bp"]
+  lₘ=63 #,              [description="Length of MG and T500 terminator"], # =u"bp"]
+  lₚ=2892 #,            [description="Length of plasmid"], # =u"bp"]
+  lᵢ = 150 #,           [description="Length of intergenic region"], # =u"bp"]
+  kσₘₘ = 50e-3 #,       [description="MM constant for supercoiling hillfunctions"], # unit=u"nM"]
+  kₒₚₑₙ=0.04 #,          [description="Rate of open complex formation"], # =u"s^-1"]
+  kᵣ=1/170 #,           [description="RFP maturation rate"], # =u"s^-1"]
+  kaₗ=6e3 #,             [description="Rate of DNA-free apolacI IPTG binding"], # =u"nM^-1*s^-1"]
+  kuaₗ=1 #,              [description="Rate of apolacI IPTG disassociation"], #  =u"s^-1"]
+  kbindₗ=10 #,           [description="lacI-promoter asossiation rate"], # =u"s^-1"]
+  kuₗ=0.022 #,           [description="lacI-promoter disassociation rate"], # =u"s^-1"]
+  kaₜ=6e3 #,             [description="aTc-TetR association rate"], # =u"nM^-1*s^-1"]
+  kuaₜ=1 #,              [description="aTc-TetR disassociation rate"], # =u"s^-1"]
+  kbindₜ=10 #,           [description="tetR-DNA association rate"], # =u"s^-1"]
+  kuₜ=0.022 #,           [description="tetR-DNA disassociation rate"], # =u"s^-1"]
+  ρₗ=0 #,                [description="Rate of lacI production"], # =u"nM*s^-1"]
+  ρₜ=0 #,                [description="Rate of tetR production"], # =u"nM*s^-1"]
+  δₛ=log(2)/(30*60) #,   [description="mSpinach degredation rate"], # =u"s^-1"]
+  δₘ=log(2)/(60*60) #,   [description="MG degredation rate"], # =u"s^-1"]
+  δₚ=0 #,                [description="Average protein degredation rate"], # =u"s^-1"]
+  σ₀=-0.065 #,           [description="Natural B-form DNA supercoil state"], # =u"turn*bp^-1"] 
+  σspₗ = σ₀*lₚ/lₗ #,       [description="Approximate Optimal supercoiling density, plac"], # =2*π*u"rad"]
+  σstₛ = σ₀*lₚ/lₛ #,      [description="Approximate Optimal supercoiling density, plac"], # =2*π*u"rad"]
+  σspₜ = σ₀*lₚ/lₜ #,      [description="Approximate Optimal supercoiling density, pTet"], # =2*π*u"rad"]
+  σstₘ = σ₀*lₚ/lₘ #,     [description="Approximate Optimal supercoiling density, pTet"], # =2*π*u"rad"]
+  fudge = 1 #,           [description="Fudge Factor"], # =u"nM"]
+  gyr₀=18.93 #, [description="Concentration Gyrase"], # =u"μM"]
+  topo₀=2 #, [description="Conc Topoisomerase"], # =u"μM"]
+  τ=0.5 #, [description="Rate of topoisomerase activity"], # =u"s^-1"]
+  γ=0.5 #, [description="Rate of Gyrase activit"], # =u"s^-1"]
+  kσₘₘ=200 #, [description="Michaelis-Menten constant for gyrase"] # =u"μM"]
 end
-@variables(begin
-  t, [unit=u"s"]
-  σtₛ(t), [description="Supercoiling State of mSpinach"], 
-  σtₘ(t), [description="Supercoiling State of MG"],
-  σpₗ(t), [description="Supercoiling density of plac"],
-  σpₜ(t), [description="Supercoiling density of pTet"],
-  pₗ(t), [description="conc plac", unit=u"nM"],
-  pₗc(t), [description="conc plac-lacI complex", unit=u"nM"],
-  pₜ(t), [description="conc pTet", unit=u"nM"],
-  pₜc(t), [description="conc pTet-TetR complex", unit=u"nM"],
-  Cₛ(t), [description="conc mSpinach", unit=u"nM"],
-  Cₘ(t), [description="conc MG", unit=u"nM"],
-  ECₛ(t), [description="conc Open Complex for mSpinach", unit=u"nM"],
-  CCₛ(t), [description="conc Closed Complex for mSpinach", unit=u"nM"],
-  ECₘ(t), [description="conc Open Complex for MG", unit=u"nM"],
-  CCₘ(t), [description="conc Closed Complex for MG", unit=u"nM"],
-  σ₊(t), [description="Strictly positive compoenent of superoil"], 
-  σ₋(t), [description="Strictly negative compoenent of superoil"], 
-  Δₖᵢₙₖ(t), [description="kink formed from super coils.", unit=u"bp"],
-  nfₛ(t), [description="Length between plac and the kink", unit=u"bp"],
-  nfₘ(t), [description="Length between pTet and the kink",unit=u"bp"] 
-end)
+@variables begin
+  σtₛ(t)=σ₀#, [description="Supercoiling State of mSpinach"], # =2*π*u"rad"], 
+  σtₘ(t)=σ₀ #, [description="Supercoiling State of MG"], # =2*π*u"rad"],
+  σpₗ(t)=σ₀#, [description="Supercoiling density of plac"], # =2*π*u"rad"],
+  σpₜ(t)=σ₀ #, [description="Supercoiling density of pTet"], # =2*π*u"rad"],
+  pₗ(t)=11 #, [description="conc plac"], # =u"nM"],
+  pₗc(t)=0 #, [description="conc plac-lacI complex"], # =u"nM"],
+  pₜ(t)=11#, [description="conc pTet"], # =u"nM"],
+  pₜc(t)=0 #, [description="conc pTet-TetR complex"], # =u"nM"],
+  Cₛ(t)=0 #, [description="conc mSpinach"], # =u"nM"],
+  Cₘ(t)=0 #, [description="conc MG"], # =u"nM"],
+  ECₛ(t)=0 #, [description="conc Open Complex for mSpinach"], # =u"nM"],
+  CCₛ(t)=0 #, [description="conc Closed Complex for mSpinach"], # =u"nM"],
+  ECₘ(t)=0 #, [description="conc Open Complex for MG"], # =u"nM"],
+  CCₘ(t)=0 #, [description="conc Closed Complex for MG"], # =u"nM"],
+  LacI(t)=0
+  aLacI(t)=0
+  TetR(t)=0
+  aTetR(t)=0
+  IPTG(t)=1
+  aTc(t) = 1
+  R(t) = 100
+  σ₊(t)=0 #, [description="Strictly positive compoenent of superoil"], # =2*π*u"rad"], 
+  σ₋(t)=0 #, [description="Strictly negative compoenent of superoil"], # unit=2*π*u"rad"], 
+  Δₖᵢₙₖ(t)=0 #, [description="kink formed from super coils."], # =u"bp"],
+  nfₛ(t)=75 #, [description="Length between plac and the kink"], # =u"bp"],
+  nfₘ(t)=75 #, [description="Length between pTet and the kink"] #,unit=u"bp"] 
+end
 
 function m(σ)
   σ₊ = (σ+abs(σ))/2
@@ -84,8 +87,8 @@ function kₑ(σt, σst)
 end
 
 eqns = [Δₖᵢₙₖ ~ (σtₛ+σtₘ)*h₀,
-        nfₛ ~ (lₗ+lₛ+lᵢ/2(pₜ/(pₜ+pₜc))+(lᵢ/2+lₘ)*pₜc/(pₜ+pₜc)-Δₖᵢₙₖ), 
-        nfₘ ~ (lₜ+lₘ+lᵢ/2(pₗ/(pₗ-pₗc))+(lᵢ/2+lₛ)*pₗc/(pₗ+pₗc)-Δₖᵢₙₖ),
+        nfₛ ~ (lₗ+lₛ+lᵢ/2(pₜ/(pₜ+pₜc))+((lᵢ/2)+lₘ)*pₜc/(pₜ+pₜc)-Δₖᵢₙₖ), 
+        nfₘ ~ (lₜ+lₘ+lᵢ/2(pₗ/(pₗ-pₗc))+((lᵢ/2)+lₛ)*pₗc/(pₗ+pₗc)-Δₖᵢₙₖ),
         D(σtₛ) ~ -(D(Cₛ)-δₛ*Cₛ-D(ECₛ))*(lₛ)/(2*h₀*nfₛ)-(D(ECₛ)-D(CCₛ))*(lₗ/2*h₀*nfₛ)+m(σtₛ),
         D(σpₗ) ~ -(D(ECₛ)-D(CCₛ))*(lₗ/2*h₀*nfₛ)+m(σpₗ),
         D(σtₘ) ~ -(D(Cₘ)-δₛ*Cₘ-D(ECₘ))*(lₘ)/(2*h₀*nfₘ)-(D(ECₘ)-D(CCₘ))*(lₜ/2*h₀*nfₘ)+m(σtₘ),
@@ -94,26 +97,23 @@ eqns = [Δₖᵢₙₖ ~ (σtₛ+σtₘ)*h₀,
 @named odesys = ODESystem(eqns,t)
 
 rxn = @reaction_network begin
-  ρₗ, 0-->LacI
-  δₚ, LacI --> 0
-  kₐₗ, + LacI +IPTG --> aLacI
-  kᵤₐₗ, aLacI --> LacI+IPTG
-  kₛₗ, pₗ + LacI --> pₗC
-  kᵤₗ, pₗC --> LacI +pₗ
-  kᵢₙᵢₜ(σ, σpsₗ), R + pₗ --> CCₛ
+  #@variables σpₗ(t) σpₜ(t) σₛ(t) σₘ(t)
+  (ρₗ, δₚ), ∅ <-->LacI
+  (kₐₗ,kᵤₐₗ), LacI +IPTG <--> aLacI
+  (kₛₗ, kᵤₗ), pₗ + LacI <--> pₗc + IPTG
+  (kᵢₙᵢₜ($σpₗ, $σspₗ), kᵣ), R + pₗ --> CCₛ
   kₒₚₑₙ, CCₛ --> ECₛ
-  kₑ(σ, σtsₛ), ECₛ --> Cₛ+R+pₗ
-  δₛ, cₛ --> 0
-  ρₜ, 0 --> TetR
-  δₚ, TetR --> 0
-  kₐₜ, TetR + aTc --> aTetR 
-  kᵤₐₜ, aTetR --> aTc + TetR
-  kₛₜ, pₜ + TetR --> pₜC 
-  kₐₜ, pₜC + aTc --> pₜ+aTetR
-  kᵢₙᵢₜ(σ,σpmₜ), R + pₜ --> CCₘ
-  kᵣ, CCₘ --> pₜ + R 
+  kₑ($σtₛ, $σstₛ), ECₛ --> Cₛ+R+pₗ
+  δₛ, Cₛ --> ∅
+  (ρₜ, δₚ), ∅ <--> TetR
+  (kₐₜ, kᵤₐₜ), TetR + aTc <--> aTetR 
+  (kₛₜ, kᵤₜ), pₜ + TetR <--> pₜc + aTc 
+  (kᵢₙᵢₜ($σpₜ, $σspₜ), kᵣ), R + pₜ <--> CCₘ
   kₒₚₑₙ, CCₘ --> ECₘ
-  kₑ(σ, σtmₘ), ECₘ --> Cₘ + R +pₜ
-  δₘ, Cₘ --> 0
+  kₑ($σtₘ, σstₘ), ECₘ --> Cₘ + R +pₜ
+  δₘ, Cₘ --> ∅
 end
-convert(ODESystem, rxn)
+@named sys = extend(odesys, rxn)
+
+odeprob = ODEProblem(sys, [], [0,3*60*60])
+
